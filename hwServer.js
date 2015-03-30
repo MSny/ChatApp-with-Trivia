fed61106515d2819f577e654e5a8cb675959cf0b
Meir <meir@Meirs-Mac.local>
2015-03-30 05:55:26 -0700

    added help and play again commands

--- hwServer.js
+++ hwServer.js
@@ -18,13 +18,17 @@ var clinton = new Hint("clinton", "plays a musical instrument", "faced impeachme
 var nixon = new Hint("nixon", "was known to be tricky", "presided over a war", "sucessfully impeached", "president in the year 3004");
 var jackson = new Hint("jackson", "shot in the chest in a duel", "was against the federal bank", "known for his kitchen cabinet", "is on the 20 dollar bill")
 var roosevelt = new Hint("roosevelt", "started his own political party", "known for his mountainering", "was played by robin williams", "was shot during a speech, and kept speaking");
-var randomizer = Math.floor(Math.random()* 5)
-var presidentialArray = [obama, clinton, nixon, jackson, roosevelt];
-var randomPresident = (presidentialArray[randomizer]);
-var count = 0;
+// var randomizer = Math.floor(Math.random()* 5)
+// var presidentialArray = [obama, clinton, nixon, jackson, roosevelt];
+// var randomPresident = (presidentialArray[randomizer]);
+// var count = 0;
 
 var server = net.createServer(function(socket) {
   console.log('client connected');
+  var randomizer = Math.floor(Math.random()* 5)
+var presidentialArray = [obama, clinton, nixon, jackson, roosevelt];
+var randomPresident = (presidentialArray[randomizer]);
+var count = 0;
 
 
   socket.write("welcome, the message history is " + messageLog.join(" \n").toString());
@@ -34,8 +38,23 @@ var server = net.createServer(function(socket) {
     console.log(data.toString().trim());
     messageLog.push(data.toString().trim());
     socket.write(data.toString().trim());
-    console.log(randomPresident);
+   // console.log(randomPresident);
 
+    // broadcast message 
+//       function broadcast(message, sender) {
+//         clients.forEach(function (client) {
+//             if(client === sender) return;
+//             client.write(message);
+//           }
+//  )}
+// broadcast(data.toString().trim() );
+// Help prompt
+if (data.toString().trim() === "help" ) {
+       socket.write(cowsay.say({
+    text : "nice to meat you, if you type more cowbell I'll play trivia with you" ,
+
+}));
+     } 
     // cows playing presidential trivia
       if (data.toString().trim() === "more cowbell" ) {
         count ++;
@@ -73,13 +92,22 @@ socket.write(cowsay.say({
 }));
      } 
      else if (data.toString().trim() === randomPresident.name ) {
-      
+      count = 0;
 socket.write(cowsay.say({
     text : "You've guessed right! (╯°□°）╯︵ ┻━┻" ,
 
 }));
      } 
+// play again
+if (data.toString().trim() === "play again" ) {
+        count = 0;
+        randomPresident;
+         randomizer;
+       socket.write(cowsay.say({
+    text : "Let's play again! type more cowbell to start another game" ,
 
+}));
+     } 
     
 
   socket.on('end', function() {
